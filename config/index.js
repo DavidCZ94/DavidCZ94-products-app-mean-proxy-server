ad42f564abfd22af73a59acfd759fcf9f9083d4a
DavidCZ94 <david940103@gmail.com>
2021-03-05 19:39:02 -0500

    sing-in, sign-up, create and deletete products with authorization complete

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 
 const config = {
-    dec: process.env.NODE_ENV !== 'production',
+    dev: process.env.NODE_ENV !== 'production',
     port: process.env.PORT || 8000,
     apiUrl: process.env.API_URL,
-    apiKeyToken: process.env.API_KEY_TOKEN
+    apiKeyToken: process.env.ADMIN_API_KEY_TOKEN
 }
 
 module.exports = { config: config };
--- index.js
+++ index.js
@@ -1,29 +1,113 @@
-const express = require("express");
+const express = require('express');
+const passport = require('passport');
+const boom = require('@hapi/boom');
+const cookieParser = require('cookie-parser');
+const axios = require('axios');
 
-const { config } = require("./config");
+const THIRTY_DAYS_IN_SEC = 2592000000;
+const TWO_HOURS_IN_SEC = 7200000;
+
+const { config } = require('./config');
 
 const app = express();
 
-
 // body parser
 app.use(express.json());
+app.use(cookieParser());
 
-app.post('/auth/sign-in', async function(req, res, next){
-    	
+// Basic Strategy
+require('./utils/auth/strategies/basic');
+
+app.post('/auth/sign-in', async function (req, res, next) {
+
+  const { rememberMe } = req.body;
+
+  passport.authenticate('basic', function(error, data){
+    try {
+        if(error || !data){
+            next(boom.unauthorized());
+        }
+
+        req.login(data, { session: false }, async function(error){
+            if(error){
+                next(error);
+            }
+            
+            const { token, ...user } = data;
+
+            res.cookie('token', token, {
+                httpOnly: !config.dev,
+                secure: !config.dev,
+                maxAge: rememberMe ? THIRTY_DAYS_IN_SEC : TWO_HOURS_IN_SEC
+            });
+
+            res.status(200).json(user);
+        });
+
+    } catch (error) {
+        next(error);
+    }
+  })(req, res, next);
 });
 
-app.post('/auth/sign-up', function(req, res, next){
+app.post('/auth/sign-up', async function (req, res, next) {
+    const { body: user } = req;
+    try{
+        await axios({
+            url: `${config.apiUrl}/api/auth/sign-up`,
+            method: 'post',
+            data: user
+        });
 
+        res.status(201).json({ messaje: 'user created' });
+    }catch(error){
+        next(error);
+    }
 });
 
-app.get('/products', function(req, res, next){
+//Create product
+app.post('/products', async function (req, res, next) {
+    try {
+        const { body: product } = req;
+        const { token } = req.cookies;
+        const { data, status } = await axios({
+            url: `${config.apiUrl}/api/products`,
+            headers: { Authorization: `Bearer ${token}`},
+            method: 'post',
+            data: product
+        });
+        if( status !== 201 ){
+            return next(boom.badImplementation());
+        }
+        res.status(201).json(data);
 
+    } catch (error) {
+        next(error);
+    }
 });
 
-app.get('/products/:productId', function(req, res, next){
+//delet product by id
+app.delete('/products/:productId', async function (req, res, next) {
+    try {
+        const { productId } = req.params;
+        const { token } = req.cookies;
+        const { data, status } = await axios({
+            url: `${config.apiUrl}/api/products/${productId}`,
+            headers: { Authorization: `Bearer ${token}`},
+            method: 'delete',
+            data: productId
+        });
 
+        if( status !== 200 ){
+            return next(boom.badImplementation());
+        }
+        res.status(201).json(data);
+
+    } catch (error) {
+        next(error);
+    }
 });
 
-app.listen(config.port, function() {
-    console.log(`Listening http://localhost:${config.port}`);
+app.listen(config.port, function () {
+  console.log(`Listening http://localhost:${config.port}`);
 });
