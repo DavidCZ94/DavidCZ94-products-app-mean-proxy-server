228d3e81fe038cff9e9539e4a832635050abd7f6
DavidCZ94 <david940103@gmail.com>
2021-04-13 20:00:46 -0500

    Search by product name added

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const cookieParser = require('cookie-parser');
 const axios = require('axios');
 const cors = require('cors');
 
-const THIRTY_DAYS_IN_SEC = 2592000000;
-const TWO_HOURS_IN_SEC = 7200000;
+const THIRTY_DAYS_IN_SEC = 60 * 60 * 24 * 30 * 1000;
+const TWO_HOURS_IN_SEC = 60 * 60 * 2 * 1000;
 
 const { config } = require('./config');
 
@@ -34,17 +34,16 @@ app.post('/auth/sign-in', async function (req, res, next) {
         req.login(data, { session: false }, async function(error){
             if(error){
                 next(error);
+            }else{
+                const { token, ...user } = data;
+                res.cookie('token', token, {
+                    httpOnly: !config.dev,
+                    secure: !config.dev,
+                    maxAge: rememberMe ? THIRTY_DAYS_IN_SEC : TWO_HOURS_IN_SEC
+                });
+                
+                res.status(200).json(data);
             }
-            
-            const { token, ...user } = data;
-
-            res.cookie('token', token, {
-                httpOnly: !config.dev,
-                secure: !config.dev,
-                maxAge: rememberMe ? THIRTY_DAYS_IN_SEC : TWO_HOURS_IN_SEC
-            });
-
-            res.status(200).json(data);
         });
 
     } catch (error) {
@@ -68,11 +67,55 @@ app.post('/auth/sign-up', async function (req, res, next) {
     }
 });
 
+// get all products
+app.get('/products', async function(req, res, next){
+    try{
+        //const { token } = req.cookies;
+        const token = req.headers.token;
+        let search = req.query.searchBy || '';
+        console.log(search);
+        const { data, status } = await axios({
+            url: `${config.apiUrl}/api/products?searchBy=${search}`,
+            headers: { Authorization: `Bearer ${token}`},
+            method: 'get',
+            responseType: 'json'
+        });
+        if ( status !== 200 ){
+            return next(boom.unauthorized());
+        }
+        res.status(200).send(data);
+    }catch(error){
+        next(error);
+    }
+});
+
+// get product by id
+app.get('/products/:productId', async function(req, res, next){
+    try {
+        //const { token } = req.cookies;
+        const { productId } = req.params;
+        const token = req.headers.token;
+        const { data, status } = await axios({
+            url: `${config.apiUrl}/api/products/${productId}`,
+            headers: { Authorization: `Bearer ${token}`},
+            method: 'get',
+            responseType: 'json'
+        });
+        if( status !== 200 ){
+            return next(boom.unauthorized());
+        }
+        res.status(200).send(data);
+    } catch (error) {
+        next(error);
+    }
+})
+
 //Create product
 app.post('/products', async function (req, res, next) {
     try {
         const { body: product } = req;
-        const { token } = req.cookies;
+        const token = req.headers.token;
+        //const { token } = req.cookies;
         const { data, status } = await axios({
             url: `${config.apiUrl}/api/products`,
             headers: { Authorization: `Bearer ${token}`},
@@ -89,11 +132,12 @@ app.post('/products', async function (req, res, next) {
     }
 });
 
-//delet product by id
+//delete product by id
 app.delete('/products/:productId', async function (req, res, next) {
     try {
+        //const { token } = req.cookies;
         const { productId } = req.params;
-        const { token } = req.cookies;
+        const token = req.headers.token;
         const { data, status } = await axios({
             url: `${config.apiUrl}/api/products/${productId}`,
             headers: { Authorization: `Bearer ${token}`},
@@ -106,6 +150,30 @@ app.delete('/products/:productId', async function (req, res, next) {
         }
         res.status(201).json(data);
 
+    } catch (error) {
+        next(error);
+    }
+});
+
+//Update product
+app.put('/products/:productId', async function (req, res, next){
+    try {
+        //const { token } = req.cookies;
+        const token = req.headers.token;
+        const { body: product } = req;
+        const { productId } = req.params;
+        const { data, status } = await axios({
+            url: `${config.apiUrl}/api/products/${productId}`,
+            headers: { Authorization: `Bearer ${token}`},
+            method: 'put',
+            data: product
+        });
+        console.log(data);
+        console.log(status);
+        if( status ==! 200 ){
+            return next(boom.badImplementation());
+        }
+        res.status(200).json(data);
     } catch (error) {
         next(error);
     }
