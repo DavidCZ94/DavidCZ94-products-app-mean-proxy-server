5e5f11adf543a59c3030f3fa6f680bbd9e2360c8
DavidCZ94 <david940103@gmail.com>
2021-05-29 18:08:00 -0500

    pagination actions to users added

--- index.js
+++ index.js
@@ -155,8 +155,10 @@ app.get('/users', async function (req, res, next) {
   try {
     const token = req.headers.token;
     let search = req.query.searchBy || '';
+    const pageNumber = req.query.pageNumber;
+    const nPerPage = req.query.nPerPage;
     const { data, status } = await axios({
-      url: `${config.apiUrl}/api/users?searchBy=${search}`,
+      url: `${config.apiUrl}/api/users?searchBy=${search}&pageNumber=${pageNumber}&nPerPage=${nPerPage}`,
       headers: { Authorization: `Bearer ${token}` },
       method: 'get',
       responseType: 'json',
