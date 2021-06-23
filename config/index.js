dc6a5dee426c53c65012b3fd284756ff49d10903
DavidCZ94 <david940103@gmail.com>
2021-06-23 18:16:48 -0500

    isActive for products added

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const config = {
-    dev: process.env.NODE_ENV !== 'production',
+    dev: process.env.NODE_ENV.trim() !== 'production',
     port: process.env.PORT || 8000,
     apiUrl: process.env.API_URL,
     apiKeyToken: process.env.ADMIN_API_KEY_TOKEN
